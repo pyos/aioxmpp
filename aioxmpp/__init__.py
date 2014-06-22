8f56961ece629036fb1d095e449b1a2a597fda39
pyos <pyos100500@gmail.com>
2014-06-22 20:30:41 +0400

    Import some more stuff into the root namespace.

--- aioxmpp/__init__.py
+++ aioxmpp/__init__.py
@@ -1,2 +1,4 @@
 import dg
-from .xmpp import client, XMPPError, XMPPMessage, XMPPPresence
+from .events import EventSystem
+from .xmlio  import XMLWriter, XMLProtocol, Node
+from .xmpp   import client, Client, JabberID, XMPPError, XMPPMessage, XMPPPresence
